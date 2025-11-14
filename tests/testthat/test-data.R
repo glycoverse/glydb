a5c4f9c7362f00723cd0d8e02c3591298365651a
fubin1999 <65430559+fubin1999@users.noreply.github.com>
2025-11-14 20:06:22 +0800

    tests: update tests

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -1,7 +1,3 @@
-test_that("fully_determined_glycans is available", {
-  expect_true(tibble::is_tibble(fully_determined_glycans))
-})
-
-test_that("topology_glycans is available", {
-  expect_true(tibble::is_tibble(topology_glycans))
+test_that("glydb_data is available", {
+  expect_true(tibble::is_tibble(glydb_data))
 })

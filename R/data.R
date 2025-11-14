42a5cc4d1fc7f1e8573d7a4b5596f1b979b3e57d
fubin1999 <65430559+fubin1999@users.noreply.github.com>
2025-11-14 19:55:14 +0800

    feat: reorganize data
    - Remove `topology_glycans`.
    - Rename `fully_determined_glycans` into `glydb_data`.

--- R/data.R
+++ R/data.R
@@ -1,6 +1,6 @@
-#' Fully Determined GlyTouCan Glycan Structures
+#' Fully Determined GlyTouCan Glycan Data
 #'
-#' A curated dataset of fully determined glycan structures from GlyTouCan.
+#' A curated dataset of fully determined glycans from GlyTouCan.
 #' "Fully determined" means that all linkages, substituents, anomers,
 #' and monosaccharides are fully specified.
 #' The dataset is derived from the GlyTouCan v2.9.1 release,
@@ -12,19 +12,4 @@
 #' - `glycan_composition`: Glycan composition (glyrepr::glycan_composition()).
 #' - `species`: Specie names. Unknown species are NAs.
 #' @source <https://data.glygen.org>
-"fully_determined_glycans"
-
-#' GlyTouCan Glycan Structures with Only Topology Information
-#'
-#' This is a simplified version of `fully_determined_glycans`,
-#' removing all linkage information,
-#' e.g. `Fuc(??-?)Gal(??-?)[Fuc(??-?)]GlcNAc(??-`.
-#' The dataset is derived from the GlyTouCan v2.9.1 release,
-#' with 2780 glycan structures currently available.
-#'
-#' @format A tibble with 2780 rows and 4 variables:
-#' - `glycan_structure`: Glycan structure (glyrepr::glycan_structure()).
-#' - `glycan_composition`: Glycan composition (glyrepr::glycan_composition()).
-#' - `species`: Specie names. Unknown species are NAs.
-#' @source <https://data.glygen.org>
-"topology_glycans"
+"glydb_data"

8b408d9b739c02d07db52b0ba8d9d6436dda4bcc
fubin1999 <65430559+fubin1999@users.noreply.github.com>
2025-11-22 16:40:51 +0800

    feat: update internal data to include glycan types

--- data-raw/internal_data.R
+++ data-raw/internal_data.R
@@ -2,44 +2,61 @@ devtools::load_all()
 library(tidyverse)
 library(glyrepr)
 
-#' Combine species names separated by semicolons
+#' Combine terms separated by semicolons
 #'
 #' Input: c("Homo sapiens;Mus musculus", "Mus musculus;Rattus norvegicus")
 #' Output: "Homo sapiens;Mus musculus;Rattus norvegicus"
 #'
-#' @param species A character vector of species names separated by semicolons.
-#' @returns A character vector of species names.
+#' @param x A character vector of terms separated by semicolons.
+#' @returns A character vector of terms.
 #' @noRd
-combine_species <- function(species) {
-  species <- species[!is.na(species)]
-  if (length(species) == 0) {
+combine_terms <- function(x) {
+  x <- as.character(x)
+  x <- x[!is.na(x)]
+  if (length(x) == 0) {
     return(NA_character_)
   }
-  res <- unique(unlist(strsplit(species, ";")))
+  res <- unique(unlist(strsplit(x, ";")))
   res <- res[!is.na(res)]
   paste(res, collapse = ";")
 }
 
 concrete_comps <- glydb_data |>
-  summarise(species = combine_species(species), .by = glycan_composition)
+  summarise(
+    species = combine_terms(species),
+    glycan_type = combine_terms(glycan_type),
+    .by = glycan_composition
+  )
 
 generic_comps <- glydb_data |>
   mutate(glycan_composition = convert_to_generic(glycan_composition)) |>
-  summarise(species = combine_species(species), .by = glycan_composition)
+  summarise(
+    species = combine_terms(species),
+    glycan_type = combine_terms(glycan_type),
+    .by = glycan_composition
+  )
 
 intact_strucs <- glydb_data |>
-  select(glycan_structure, species)
+  select(glycan_structure, species, glycan_type)
 
 topological_strucs <- glydb_data |>
   mutate(glycan_structure = remove_linkages(glycan_structure)) |>
-  summarise(species = combine_species(species), .by = glycan_structure)
+  summarise(
+    species = combine_terms(species),
+    glycan_type = combine_terms(glycan_type),
+    .by = glycan_structure
+  )
 
 basic_strucs <- glydb_data |>
   mutate(
     glycan_structure = convert_to_generic(glycan_structure),
     glycan_structure = remove_linkages(glycan_structure)
   ) |>
-  summarise(species = combine_species(species), .by = glycan_structure)
+  summarise(
+    species = combine_terms(species),
+    glycan_type = combine_terms(glycan_type),
+    .by = glycan_structure
+  )
 
 usethis::use_data(
   concrete_comps,
@@ -47,5 +64,6 @@ usethis::use_data(
   intact_strucs,
   topological_strucs,
   basic_strucs,
-  internal = TRUE
+  internal = TRUE,
+  overwrite = TRUE
 )

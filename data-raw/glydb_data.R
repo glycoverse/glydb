ec5ce0248fa642b04fa9017babd1999f147148ea
fubin1999 <65430559+fubin1999@users.noreply.github.com>
2025-11-22 15:48:01 +0800

    feat: add a glycan_type column to glydb_data

--- data-raw/glydb_data.R
+++ data-raw/glydb_data.R
@@ -32,7 +32,30 @@ species_prepared <- species |>
   select(glytoucan_ac, species = tax_name) |>
   summarise(species = str_c(unique(species), collapse = ";"), .by = glytoucan_ac)
 
+get_glycan_type <- function(glycan_structure) {
+  motifs <- c(
+    "N" = "Man(b1-4)GlcNAc(b1-4)GlcNAc(b1-",
+    "O-GalNAc" = "GalNAc(a1-",
+    "O-GlcNAc" = "GlcNAc(b1-",
+    "O-Man" = "Man(a1-",
+    "O-Fuc" = "Fuc(a1-",
+    "O-Glc" = "Glc(b1-"
+  )
+  motif_mat <- glymotif::have_motifs(glycan_structure, motifs, alignment = "core")
+  res <- dplyr::case_when(
+    motif_mat[, "N"] ~ "N",
+    motif_mat[, "O-GalNAc"] ~ "O-GalNAc",
+    motif_mat[, "O-GlcNAc"] ~ "O-GlcNAc",
+    motif_mat[, "O-Man"] ~ "O-Man",
+    motif_mat[, "O-Fuc"] ~ "O-Fuc",
+    motif_mat[, "O-Glc"] ~ "O-Glc",
+    .default = NA_character_
+  )
+  factor(res, levels = c("N", "O-GalNAc", "O-GlcNAc", "O-Man", "O-Fuc", "O-Glc"))
+}
+
 glydb_data <- iupac_prepared |>
-  left_join(species_prepared, by = "glytoucan_ac")
+  left_join(species_prepared, by = "glytoucan_ac") |>
+  mutate(glycan_type = get_glycan_type(glycan_structure))
 
 usethis::use_data(glydb_data, overwrite = TRUE)

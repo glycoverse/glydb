18c444c8b17d67f05775e3245c2f54c51e60ae8c
fubin1999 <65430559+fubin1999@users.noreply.github.com>
2026-02-06 21:37:49 +0800

    feat: update internal data to use GlyGen Datasets v2.10.1

--- data-raw/glydb_data.R
+++ data-raw/glydb_data.R
@@ -2,28 +2,28 @@ library(tidyverse)
 library(glyparse)
 library(glyrepr)
 
-accessions <- read_csv("data-raw/glycan_fully_determined_v2_9_1.csv")
-iupac <- read_csv("data-raw/glycan_sequences_iupac_extended_v2_9_1.csv")
-species <- read_csv("data-raw/glycan_species_v2_9_1.csv")
+accessions <- read_csv("data-raw/glycan_fully_determined_v2_10_1.csv")
+iupac <- read_csv("data-raw/glycan_sequences_iupac_condensed_v2_10_1.csv")
+species <- read_csv("data-raw/glycan_species_v2_10_1.csv")
 
 iupac_temp <- iupac |>
   semi_join(accessions, by = "glytoucan_ac") |>
-  filter(!str_ends(sequence_iupac_extended, "ol")) |>
-  filter(!str_detect(sequence_iupac_extended, "f"))
+  filter(!str_ends(sequence_iupac_condensed, "ol")) |>
+  filter(!str_detect(sequence_iupac_condensed, "f"))
 
-dry_try_parse <- function(x) {
-  safe_parse <- purrr::possibly(parse_iupac_extended, NA)
-  result <- purrr::map(x, safe_parse)
-  n_total <- length(x)
-  n_success <- sum(!is.na(result))
-  cli::cli_inform("Parsed {n_success} out of {n_total} glycans")
-  !is.na(result)
-}
+# dry_try_parse <- function(x) {
+#   safe_parse <- purrr::possibly(parse_iupac_condensed, NA)
+#   result <- purrr::map(x, safe_parse)
+#   n_total <- length(x)
+#   n_success <- sum(!is.na(result))
+#   cli::cli_inform("Parsed {n_success} out of {n_total} glycans")
+#   !is.na(result)
+# }
+# good_iupac <- dry_try_parse(iupac_temp$sequence_iupac_condensed)
 
-good_iupac <- dry_try_parse(iupac_temp$sequence_iupac_extended)
-iupac_prepared <- iupac_temp[good_iupac, ] |>
+iupac_prepared <- iupac_temp |>
   mutate(
-    glycan_structure = parse_iupac_extended(sequence_iupac_extended),
+    glycan_structure = as_glycan_structure(sequence_iupac_condensed),
     glycan_composition = as_glycan_composition(glycan_structure),
     .keep = "unused"
   )

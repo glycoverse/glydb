939853820a5876d89a9bc5c4f7f9334b5741d4af
fubin1999 <65430559+fubin1999@users.noreply.github.com>
2026-03-29 22:53:26 +0800

    docs: update data version in documentation

--- R/data.R
+++ R/data.R
@@ -3,23 +3,10 @@
 #' A curated dataset of fully determined glycans from GlyTouCan.
 #' "Fully determined" means that all linkages, substituents, anomers,
 #' and monosaccharides are fully specified.
-#' The dataset is derived from the GlyTouCan v2.9.1 release,
-#' with 6960 glycan structures currently available.
+#' The dataset is derived from the GlyTouCan v2.10.1 release,
+#' with 7,074 glycan structures currently available.
 #'
-#' @details
-#' # Duplicate Glycan Structures
-#'
-#' There are duplicate glycan structures in the dataset,
-#' due to the presence of rare monosaccharides in some glycans.
-#' For example, Fuc has L configuration in most cases,
-#' but in some situations it can have D configuration.
-#' `glyparse` and `glyrepr` doesn't support this kind of ambiguity for now,
-#' so we keep them all in the dataset.
-#' However, this situation is rare, and we have 6853 unique glycan structures in the dataset.
-#' If you need the detailed information, you can use the `glytoucan_ac` (GlyTouCan accession)
-#' to query the original data.
-#'
-#' @format A tibble with 6960 rows and 5 variables:
+#' @format A tibble with 7,074 rows and 5 variables:
 #' - `glytoucan_ac`: GlyTouCan accession.
 #' - `glycan_structure`: Glycan structure (glyrepr::glycan_structure()).
 #' - `glycan_composition`: Glycan composition (glyrepr::glycan_composition()).
